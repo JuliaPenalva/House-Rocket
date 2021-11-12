ef0260fa1662dd2c7b08c741bcdd0482e6a73d0d
Julia Penalva <78880949+JuliaPenalva@users.noreply.github.com>
2021-11-12 11:45:51 -0300

    Add files via upload

--- house_rocket.py
+++ house_rocket.py
@@ -14,15 +14,15 @@ def database(path):
 
 def show_results(data_buy):
     st.title("House Rocket - Analytics Insights")
-    st.sidebar.image("images/small logo.jpg", use_column_width=True)
+    st.sidebar.image("small logo.jpg", use_column_width=True)
 
     # creating filters
     st.sidebar.title("Filters:")
     buy_type_filter = st.sidebar.multiselect("Buy type:", data_buy['buy_type'].unique(), default = data_buy['buy_type'].unique())
     minimum_profit = st.sidebar.slider("Minimum sell profit - per house:", min_value = int(data_buy['profit'].min()),
-                                       max_value = int(data_buy['profit'].max()), value = int(data_buy['profit'].mean()), step = int(data_buy['profit'].std()))
+                                       max_value = int(data_buy['profit'].max()), value = int(data_buy['profit'].min()), step = int(data_buy['profit'].std()))
     max_price = st.sidebar.slider("Maximum house buy price - per house:", min_value = int(data_buy['price'].min()),
-                                  max_value = int(data_buy['price'].max()), value = int(data_buy['price'].min()), step = int(data_buy['price'].std()))
+                                  max_value = int(data_buy['price'].max()), value = int(data_buy['price'].max()), step = int(data_buy['price'].std()))
 
     #applying filters
     if (buy_type_filter != []):
@@ -51,10 +51,10 @@ def show_results(data_buy):
                             lon = 'long',
                             zoom = 10,
                             color = 'buy_type',
-                            size = 'profit',
+                            size = 'price',
+                            size_max= 15,
                             text= 'id',
-                            color_continuous_scale=px.colors.cyclical.Twilight)
-    fig.update_layout(mapbox_style = 'open-street-map')
+                            mapbox_style= 'open-street-map')
     fig.update_layout(height = 600, margin = {'r':0, 't':0, 'b':0, 'l':0})
     fig.update_layout(showlegend=False)
     st.plotly_chart(fig)
